c12f111554483d5b632e29ae614040872faf2e1d
muh23n3134-bit <muh23n3134@uoanbar.edu.iq>
2026-05-10 23:24:11 +0300

    Refactor ProChan to use new HTTP client and headers

--- src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/ProChan.kt
+++ src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/ProChan.kt
@@ -1,7 +1,6 @@
 package eu.kanade.tachiyomi.extension.ar.mangapro
 
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.network.asObservableSuccess
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.SManga
@@ -10,41 +9,54 @@ import keiyoushi.utils.parseAs
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
-import rx.Observable
 
 class ProChan : HttpSource() {
-    override val name = "ProChan"
-    override val lang = "ar"
-    private val domain = "procomic.pro"
-    override val baseUrl = "https://$domain"
-    override val supportsLatest = true
-    override val versionId = 5
 
-    override val client = network.cloudflareClient.newBuilder()
-        .addInterceptor(ProChanInterceptor(baseUrl))
-        .build()
+    override val name = "ProChan"
+
+    override val baseUrl = "https://procomic.pro"
+
+    override val lang = "ar"
+
+    override val supportsLatest = true
+
+    override val client = ProChanHttp.configureClient(network.cloudflareClient, baseUrl)
+
+    override fun headersBuilder() = ProChanHttp.getHeaders(baseUrl).newBuilder()
 
     override fun popularMangaRequest(page: Int): Request {
+        val sessionRequest = Request.Builder()
+            .url(baseUrl)
+            .headers(headers)
+            .build()
+        
+        try {
+            client.newCall(sessionRequest).execute().close()
+        } catch (_: Exception) {}
+
         val url = "$baseUrl/api/public/series/search".toHttpUrl().newBuilder().apply {
             addQueryParameter("status", "approved")
             addQueryParameter("limit", "18")
             addQueryParameter("page", page.toString())
             addQueryParameter("sort", "views")
         }.build()
+        
         return GET(url, headers)
     }
 
     override fun popularMangaParse(response: Response): MangasPage {
         val data = response.parseAs<MetaData<BrowseManga>>()
+        
         val mangas = data.data.map { manga ->
             SManga.create().apply {
                 url = "/series/${manga.type}/${manga.id}/${manga.slug}"
                 title = manga.title
                 thumbnail_url = (manga.coverImageApp?.desktop ?: manga.coverImage)?.let {
-                    if (it.startsWith("/")) "https://${manga.cdn ?: "cdn"}.$domain$it" else it
+                    if (it.startsWith("/")) "https://${manga.cdn ?: "cdn"}.procomic.pro$it" else it
                 }
             }
         }
+        
         return MangasPage(mangas, data.meta.hasNextPage())
     }
 
@@ -55,6 +67,7 @@ class ProChan : HttpSource() {
             addQueryParameter("page", page.toString())
             addQueryParameter("sort", "latest")
         }.build()
+        
         return GET(url, headers)
     }
 
@@ -67,13 +80,17 @@ class ProChan : HttpSource() {
             addQueryParameter("page", page.toString())
             if (query.isNotBlank()) addQueryParameter("search", query)
         }.build()
+        
         return GET(url, headers)
     }
 
     override fun searchMangaParse(response: Response) = popularMangaParse(response)
 
     override fun mangaDetailsParse(response: Response) = throw UnsupportedOperationException()
+
     override fun chapterListParse(response: Response) = throw UnsupportedOperationException()
+
     override fun pageListParse(response: Response) = throw UnsupportedOperationException()
+
     override fun imageUrlParse(response: Response) = throw UnsupportedOperationException()
 }

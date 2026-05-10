5d8b59247668ac3d2b0dc478bd1247138f456a8f
muh23n3134-bit <muh23n3134@uoanbar.edu.iq>
2026-05-10 23:22:03 +0300

    Add SimpleCookieJar for cookie management

--- src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/ProChanHttp.kt
+++ src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/ProChanHttp.kt
@@ -1,5 +1,8 @@
 package eu.kanade.tachiyomi.extension.ar.mangapro
 
+import okhttp3.Cookie
+import okhttp3.CookieJar
+import okhttp3.HttpUrl
 import okhttp3.Headers
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
@@ -12,7 +15,7 @@ object ProChanHttp {
             .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,application/json,*/*;q=0.8")
             .add("Accept-Language", "ar,en-US;q=0.9,en;q=0.8")
             .add("Referer", "$baseUrl/")
-            .add("Origin", baseUrl)
+            .add("Cache-Control", "max-age=0")
             .add("Sec-Ch-Ua", "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"")
             .add("Sec-Ch-Ua-Mobile", "?0")
             .add("Sec-Ch-Ua-Platform", "\"Windows\"")
@@ -26,9 +29,22 @@ object ProChanHttp {
 
     fun configureClient(baseClient: OkHttpClient, baseUrl: String): OkHttpClient {
         return baseClient.newBuilder()
-            .connectTimeout(15, TimeUnit.SECONDS)
-            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .cookieJar(SimpleCookieJar())
             .addInterceptor(ProChanInterceptor(baseUrl))
             .build()
+    }
+
+    class SimpleCookieJar : CookieJar {
+        private val storage = mutableListOf<Cookie>()
+
+        override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+            storage.addAll(cookies)
+        }
+
+        override fun loadForRequest(url: HttpUrl): List<Cookie> {
+            return storage.filter { it.matches(url) }
+        }
     }
 }

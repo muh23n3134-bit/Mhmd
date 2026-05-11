0371a3c75413f4f22d96cfcdb605fff5d456088d
muh23n3134-bit <muh23n3134@uoanbar.edu.iq>
2026-05-11 16:44:05 +0300

    Update ProChanDto.kt

--- src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/ProChanDto.kt
+++ src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/ProChanDto.kt
@@ -6,6 +6,36 @@ import kotlinx.serialization.Serializable
 data class Data<T>(val data: T)
 
 @Serializable
+data class NextData(val props: NextProps)
+
+@Serializable
+data class NextProps(val pageProps: PageProps)
+
+@Serializable
+data class PageProps(
+    val series: MangaDetails? = null,
+    val initialChapters: InitialChapters? = null
+)
+
+@Serializable
+data class MangaDetails(
+    val id: Int,
+    val initialChapters: InitialChapters? = null
+)
+
+@Serializable
+data class InitialChapters(
+    val initialChapters: List<Chapter>
+)
+
+@Serializable
+data class Chapter(
+    val id: Int,
+    val number: String? = null,
+    val title: String? = null
+)
+
+@Serializable
 data class Images(
     val images: List<String>,
     val maps: List<ScrambledData>? = null

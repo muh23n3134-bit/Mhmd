0b138f2cf6a18c1e03136a8ec17c6f19cb4f9804
muh23n3134-bit <muh23n3134@uoanbar.edu.iq>
2026-05-11 23:58:57 +0300

    Update ProChanParser.kt

--- src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/ProChanParser.kt
+++ src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/ProChanParser.kt
@@ -1,40 +1,92 @@
 package eu.kanade.tachiyomi.extension.ar.mangapro
 
+import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.model.Page
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
+import keiyoushi.utils.parseAs
+import okhttp3.Response
 import java.util.Calendar
 
 object ProChanParser {
-    fun parseLibrary(jsonString: String, json: Json): Pair<List<SManga>, Boolean> {
-        val res = json.decodeFromString<LibraryDto>(jsonString)
+
+    private val cdnMap = mapOf(
+        "cdn1" to "https://cdn1.prochan.net",
+        "cdn2" to "https://cdn2.prochan.net",
+        "cdn3" to "https://cdn3.prochan.net"
+    )
+
+    fun parseLibrary(response: Response): Pair<List<SManga>, Boolean> {
+        val res = response.parseAs<LibraryDto>()
         val mangas = res.library.map { item ->
             SManga.create().apply {
                 url = "/series/${item.type}/${item.id}/${item.slug}"
                 title = item.title
-                thumbnail_url = if (item.coverImage.startsWith("http")) item.coverImage else "https://app.prochan.net/series" + item.coverImage
+                thumbnail_url = if (item.coverImage.startsWith("http"))
+                    item.coverImage
+                else
+                    "https://app.prochan.net/series${item.coverImage}"
             }
         }
         return Pair(mangas, mangas.isNotEmpty())
     }
 
-    fun chapterListParse(jsonString: String, json: Json): List<SChapter> {
-        val res = json.decodeFromString<ChapterResponseDto>(jsonString)
+    fun mangaDetailsParse(response: Response): SManga {
+        val res = response.parseAs<DataDto<MangaDetailDto>>()
+        val item = res.data
+        return SManga.create().apply {
+            title = item.title ?: ""
+            description = item.description
+            author = item.author
+            thumbnail_url = item.coverImage?.let {
+                if (it.startsWith("http")) it else "https://app.prochan.net/series$it"
+            }
+            status = when (item.status?.lowercase()) {
+                "ongoing", "مستمر" -> SManga.ONGOING
+                "completed", "مكتمل" -> SManga.COMPLETED
+                "hiatus", "متوقف" -> SManga.ON_HIATUS
+                else -> SManga.UNKNOWN
+            }
+            genre = item.genres?.joinToString(", ")
+        }
+    }
+
+    fun chapterListParse(response: Response, type: String, mangaId: String): List<SChapter> {
+        val res = response.parseAs<ChapterListDto>()
         return res.data.map { item ->
             SChapter.create().apply {
-                url = "/chapter/${item.id}"
-                name = "الفصل ${item.chapterNumber ?: ""}${if (!item.title.isNullOrBlank()) " - ${item.title}" else ""}"
+                url = "/series/$type/$mangaId/manga/${item.id}/${item.chapterNumber}"
+                name = "الفصل ${item.chapterNumber}" +
+                    if (!item.title.isNullOrBlank()) " - ${item.title}" else ""
                 date_upload = Calendar.getInstance().timeInMillis
             }
         }
     }
 
-    fun pageListParse(jsonString: String, json: Json): List<Page> {
-        val res = json.decodeFromString<DataDto<ImagesDto>>(jsonString)
-        return res.data.images.mapIndexed { i, url ->
-            Page(i, "", url)
+    fun pageListFromChapterItem(item: ChapterItem, decryptionKey: String): List<Page> {
+        val cdnBase = cdnMap[item.cdnPath] ?: cdnMap["cdn1"]!!
+        val metadata = item.metadata ?: return emptyList()
+
+        if (metadata.maps.isNotEmpty() && metadata.maps.size == metadata.images.size) {
+            return metadata.maps.mapIndexed { i, map ->
+                val imagePath = metadata.images[i]
+                val scrambled = ScrambledData(token = map.token)
+                val tokenValue = ProChanSecurity.decryptImage(scrambled, decryptionKey)
+                val finalUrl = if (tokenValue != null) {
+                    "$cdnBase${imagePath}?v=${tokenValue.v}&cid=${tokenValue.cid}"
+                } else {
+                    "$cdnBase$imagePath"
+                }
+                Page(i, "", finalUrl)
+            }
         }
+
+        return metadata.images.mapIndexed { i, path ->
+            Page(i, "", "$cdnBase$path")
+        }
+    }
+
+    fun pageListParse(response: Response): List<Page> {
+        val res = response.parseAs<DataDto<ImagesDto>>()
+        return res.data.images.mapIndexed { i, url -> Page(i, "", url) }
     }
 }
